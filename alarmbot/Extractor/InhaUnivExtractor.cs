7e46864732de6427d7a0a8b60c0d530a88718d9b
rollrat <rollrat@inha.edu>
2020-03-10 22:15:17 +0900

    extractor: InhaUnivExtractor header

---
+++ alarmbot/Extractor/InhaUnivExtractor.cs
@@ -0,0 +1,21 @@
+﻿// This source code is a part of Inha Univ AlarmBot.
+// Copyright (C) 2020. rollrat. Licensed under the MIT Licence.
+
+using alarmbot.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace alarmbot.Extractor
+{
+    public class InhaUnivDBModel : SQLiteColumnModel
+    {
+        public string DateTime { get; set; }
+        public string Classify { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class InhaUnivExtractor
+    {
+    }
+}
